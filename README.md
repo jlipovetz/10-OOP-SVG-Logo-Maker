9be59349bc17cb42523b267d4b5851ffa2d17cbe
jlipovetz <JLIPOVETZ@GMAIL.COM>
2023-12-04 10:18:36 -0600

    shapes and inquirer setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 10-OOP-SVG-Logo-Maker
+![Image showing a green circle with white text that reads "SVG.".](./shape.svg)
---
+++ index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+const {generateCircle, generateTriangle, generateSquare} = require('./lib/shapes')
+const textColors = ["white", "black"]
+const shapeColors = ["blue", "red", "green"]
+const shapes = ["triangle", "circle", "square"]
+
+function newLogo(){
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Please enter up to three characters',
+    },
+    {
+      type: 'list',
+      name: 'textColor',
+      message: 'Please enter the text color',
+      choices: textColors
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Please enter the shape',
+      choices: shapes
+    },
+    {
+      type: 'list',
+      name: 'shapeColor',
+      message: 'Please enter the shape color',
+      choices: shapeColors
+    },
+  ]).then((data) => {
+    if (data.shape === "circle")
+    shapeContent = generateCircle(data);
+    else if (data.shape === "triangle")
+    shapeContent = generateTriangle(data)
+    else if (data.shape === "square")
+    shapeContent = generateSquare(data)
+
+    fs.writeFile('shape.svg', shapeContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created README!')) 
+
+    
+
+  })
+}
+
+newLogo()
