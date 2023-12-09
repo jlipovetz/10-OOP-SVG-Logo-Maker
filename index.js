7f47ed4fa98120f05ff35b5eb221e623734f9a46
jlipovetz <JLIPOVETZ@GMAIL.COM>
2023-12-09 14:33:41 -0600

    updated code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
-const {generateCircle, generateTriangle, generateSquare} = require('./lib/shapes')
+const { Circle, Triangle, Square } = require('./lib/shapes')
+
 const textColors = ["white", "black"]
 const shapeColors = ["blue", "red", "green"]
 const shapes = ["triangle", "circle", "square"]
 
-function newLogo(){
+function newLogo() {
   inquirer.prompt([
     {
       type: 'input',
@@ -32,18 +33,43 @@ function newLogo(){
     },
   ]).then((data) => {
     if (data.shape === "circle")
-    shapeContent = generateCircle(data);
-    else if (data.shape === "triangle")
-    shapeContent = generateTriangle(data)
-    else if (data.shape === "square")
-    shapeContent = generateSquare(data)
-
-    fs.writeFile('shape.svg', shapeContent, (err) =>
-    err ? console.log(err) : console.log('Successfully created README!')) 
-
-    
-
+      return fs.writeFile('shape.svg', generateCircle(data), (err) =>
+        err ? console.log(err) : console.log('Successfully created Logo!'))
+    if (data.shape === "square")
+    fs.writeFile('shape.svg', generateSquare(data), (err) =>
+      err ? console.log(err) : console.log('Successfully created Logo!'))
+    if (data.shape === "triangle")
+    fs.writeFile('shape.svg', generateTriangle(data), (err) =>
+      err ? console.log(err) : console.log('Successfully created Logo!'))
   })
 }
+
+function generateCircle(data) {
+  return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  <${data.shape} cx="150" cy="100" r="80" fill="${data.shapeColor}" />
+  <text x="150" y="115" font-size="50" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+</svg>`
+}
+
+function generateSquare(data) {
+  data.shape = "rect"
+  return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  <${data.shape} x="20" y="20" width="200" height="200" fill="${data.shapeColor}" />
+  <text x="120" y="120" font-size="50" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+</svg>`
+}
+
+function generateTriangle(data) {
+  data.shape = "polygon"
+  return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  <${data.shape} points="150, 18 244, 182 56, 182" fill="${data.shapeColor}" />
+  <text x="150" y="140" font-size="50" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+</svg>`
+}
+
+
+
+
+
 
 newLogo()
---
+++ shape.md
@@ -0,0 +1,1 @@
+![LOGO.](./shape.svg)
